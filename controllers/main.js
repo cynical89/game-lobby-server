470152e9a8c9cd8c629ff9bb6ec0bac4b1d0b4f4
Anthony Michelizzi <cynical89@gmail.com>
2016-11-11 14:13:32 -0800

    boiler-plate

---
+++ controllers/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const config = require("../config.json");
+
+let user = null;
+
+module.exports.index = function* index() {
+	if (this.isAuthenticated()) {
+		user = this.session.passport.user;
+	}
+	yield this.body = user;
+};
